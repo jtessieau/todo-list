98e3843119886c822aaa279a157a2070b719cc25
Julien TESSIEAU <julien.tessieau@gmail.com>
2022-07-25 19:33:26 +0100

    [feat] add react router to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -8,9 +9,13 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 );
