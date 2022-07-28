067f44bd6f843b501953647603faf2d285a1f3a5
Julien TESSIEAU <julien.tessieau@gmail.com>
2022-07-28 18:39:39 +0100

    [feat] add formatting before saving a task

--- frontend/src/components/Task/TaskForm.jsx
+++ frontend/src/components/Task/TaskForm.jsx
@@ -7,8 +7,16 @@ function TaskForm(props) {
         e.preventDefault();
         const taskName = document.getElementById('task-name');
 
+        if (!taskName.value.trim()) return;
+
+        let taskNameString = taskName.value.trim();
+        taskNameString =
+            taskNameString[0].toUpperCase() + taskNameString.slice(1);
+
+        console.log(taskNameString);
+
         const task = {
-            name: taskName.value,
+            name: taskNameString,
         };
         saveTask(task)
             .then(() => {
