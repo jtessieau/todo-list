fcc5221d4a2d65c12af9efa6e9f03981387b351b
Julien TESSIEAU <julien.tessieau@gmail.com>
2022-07-30 18:44:20 +0100

    [refactor] remove useless spaces

--- backend/api/v1/routes/taskRoutes.js
+++ backend/api/v1/routes/taskRoutes.js
@@ -4,11 +4,8 @@ const taskController = require('../controllers/taskController');
 const router = express.Router();
 
 router.get('/', taskController.show);
-
 router.post('/', taskController.store);
-
 router.put('/:id', taskController.update);
-
 router.delete('/:id', taskController.destroy);
 
 module.exports = router;
