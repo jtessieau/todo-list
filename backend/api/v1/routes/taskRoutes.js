813d7946c02c270e67050adcbadbc303f48ba46b
Julien TESSIEAU <julien.tessieau@gmail.com>
2022-08-01 21:56:38 +0100

    [feat] add auth middleware to the backend

--- backend/api/v1/routes/taskRoutes.js
+++ backend/api/v1/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const taskController = require('../controllers/taskController');
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/', taskController.show);
+router.get('/', auth, taskController.show);
 router.post('/', taskController.store);
 router.put('/:id', taskController.update);
 router.delete('/:id', taskController.destroy);
